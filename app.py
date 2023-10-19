6a8860924e4b2b3391d67627cd140806782a7974
Diego Leal <70763447+LealDias@users.noreply.github.com>
2023-10-19 15:36:59 +0000

    lotties

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ with st.container():
 
 with right_column:
 # Create a Lottie animation using lottie-web
-  iframe_html = f'<iframe src="lottie_json" title="Lottie Animation" frameborder="0" style="width:100%; height:500px;"></iframe>'
+  iframe_html = f'<iframe src="images/astronot.json" title="Lottie Animation" frameborder="0" style="width:100%; height:500px;"></iframe>'
   st.markdown(iframe_html, unsafe_allow_html=True)
 
 # ------- PROJECTS --------- #
